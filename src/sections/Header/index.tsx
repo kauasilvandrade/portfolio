a845ae66f4a588fd5d3e951192dc1f648f640fa0
Kauã da Silva Andrade <107755487+kauasilvandrade@users.noreply.github.com>
2026-05-22 10:27:46 -0300

    style: import fonts and colors

--- src/sections/Header/index.tsx
+++ src/sections/Header/index.tsx
@@ -3,24 +3,26 @@ import { LinkPrimary } from "../../components/LinkPrimary";
 export function Header() {
   return (
     <header>
-      <a href="#about">Kauã.</a>
-      <nav>
-        <ul>
-          <li>
-            <a href="#about">Sobre</a>
-          </li>
-          <li>
-            <a href="#skills">Habilidades</a>
-          </li>
-          <li>
-            <a href="#projects">Projetos</a>
-          </li>
-          <li>
-            <a href="#contact">Contato</a>
-          </li>
-        </ul>
-      </nav>
-      <LinkPrimary section="#about" title="Fale Comigo" />
+      <div className="container">
+        <a href="#about">Kauã.</a>
+        <nav>
+          <ul>
+            <li>
+              <a href="#about">Sobre</a>
+            </li>
+            <li>
+              <a href="#skills">Habilidades</a>
+            </li>
+            <li>
+              <a href="#projects">Projetos</a>
+            </li>
+            <li>
+              <a href="#contact">Contato</a>
+            </li>
+          </ul>
+        </nav>
+        <LinkPrimary section="#about" title="Fale Comigo" />
+      </div>
     </header>
   );
 }
